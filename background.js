90f4be51af6f5d45df596ed715d73c134bf7d5e8
Harold Martin <harold.martin@gmail.com>
2017-01-24 22:07:13 -0800

    💃

--- background.js
+++ background.js
@@ -1,3 +1,46 @@
+const NPR = "*://one.npr.org/*";
+var story_url = "";
+
+function genericOnClick(info, tab) {
+	var speed = parseFloat(info.menuItemId.split('-')[2]);
+	chrome.storage.sync.set({'speed': speed});
+	runOnNprTab(function(tabId){ chrome.tabs.sendMessage(tabId, {"action": "speed", "speed": speed}) });
+}
+
+var speeds = [0.75, 1.00, 1.25, 1.50, 1.75, 2.00];
+chrome.storage.sync.get({'speed': 1}, function(items) {
+	console.log(items.speed);
+	for (let s of speeds) {
+	  chrome.contextMenus.create({"title": (s.toFixed(2) + "×"), "id": "npr-playback-"+s, "checked": s==items.speed, "type":"radio", "contexts":["page","selection","browser_action","page_action"], "onclick": genericOnClick, "documentUrlPatterns":[NPR]});
+	}
+});
+
+
+
+chrome.browserAction.onClicked.addListener(function(tab){
+	if (tab.url.indexOf("one.npr.org") != -1) {
+		alert("Right click on the icon to set playback speed")
+	} else {
+		runOnNprTab(function(tabId){ 
+			if (tabId) {
+				chrome.tabs.sendMessage(tabId, {"action": "playpause"});
+			} else {
+				chrome.tabs.create({"pinned": true, "url" : "http://one.npr.org/"});
+			}
+		});
+	}
+});
+
+var runOnNprTab = function(callback) {
+	chrome.tabs.query({"url":NPR}, function(tabs){
+		if (tabs && tabs.length > 0) {
+			callback(tabs[0].id);
+		} else {
+			callback(null);
+		}
+	});
+};
+
 chrome.runtime.onInstalled.addListener(function() {
   console.log("Installed.");
 
@@ -8,24 +51,42 @@ chrome.runtime.onInstalled.addListener(function() {
 });
 
 chrome.runtime.onMessage.addListener(function(msg, _, sendResponse) {
-  if (msg.setAlarm) {
-    // For testing only.  delayInMinutes will be rounded up to at least 1 in a
-    // packed or released extension.
-    chrome.alarms.create({delayInMinutes: 0.1});
-  } else if (msg.delayedResponse) {
-    // Note: setTimeout itself does NOT keep the page awake. We return true
-    // from the onMessage event handler, which keeps the message channel open -
-    // in turn keeping the event page awake - until we call sendResponse.
-    setTimeout(function() {
-      sendResponse("Got your message.");
-    }, 5000);
-    return true;
-  } else if (msg.getCounters) {
-    sendResponse({counter: counter++,
-                  persistentCounter: localStorage.counter++});
-  }
-  // If we don't return anything, the message channel will close, regardless
-  // of whether we called sendResponse.
+	if (msg.action == "notification") {
+		msg.options.iconUrl = "img/favicon-192x192-padded.png";
+		msg.options.buttons = [{"title":"Next ▶▶"}];
+		if (msg.story_url) {
+			story_url = msg.story_url;
+			msg.options.buttons.push({"title":"Read Story"})
+		} else {
+			story_url = null;
+		}
+		chrome.notifications.create("npr-one", msg.options);
+	}
+	else if (msg.action == "onplay") {
+		
+	}
+	else if (msg.action == "onpause") {
+		
+	}
+});
+
+chrome.notifications.onClicked.addListener(function() {
+	runOnNprTab(function(tabId){
+		if (tabId) {
+			chrome.tabs.get(tabId, function (tab){ chrome.windows.update(tab.windowId, {focused: true}) });
+			chrome.tabs.update(tabId, {selected: true});
+		}
+	});
+	chrome.notifications.clear('npr-one');
+});
+
+chrome.notifications.onButtonClicked.addListener(function(notificationId, buttonIndex) {
+	if (buttonIndex == 0) {
+		runOnNprTab(tabId => chrome.tabs.sendMessage(tabId, {"action": "skip"}) );
+	} else if (buttonIndex == 1) {
+		chrome.tabs.create({"url" : story_url});
+	}
+	chrome.notifications.clear('npr-one');
 });
 
 chrome.alarms.onAlarm.addListener(function() {
@@ -36,4 +97,4 @@ chrome.runtime.onSuspend.addListener(function() {
   console.log("Unloading.");
 });
 
-chrome.alarms.create({delayInMinutes: 0.1});
+// chrome.alarms.create({delayInMinutes: 0.1});
--- npr.js
+++ npr.js
@@ -1,11 +1,111 @@
+var startedPlay = false;
+var settedRate;
 var player = document.getElementsByTagName('audio')[0];
-var setPlaybackRate = function() {
-	var savedRate = localStorage['playbackRate'];
-	console.log("setting rate : " + savedRate);
-	savedRate && ((player.playbackRate = savedRate));
+var $skip = function() {
+	var skip = document.getElementsByTagName('skip');
+	if (skip.length > 0) {
+		skip = skip[0];
+		var btn = skip.getElementsByTagName('button');
+		if (btn.length > 0) {
+			btn[0].click();
+		}
+	}
 };
-setPlaybackRate();
-player.autoplay="true";
-try { player.play() } catch (e) { console.log(e) };
-player.onloadeddata = setPlaybackRate;
+var $pp = function() {
+	var toggle = document.getElementsByTagName('toggle-play');
+	if (toggle.length > 0) {
+		toggle = toggle[0];
+		var btn = toggle.getElementsByTagName('button');
+		if (btn.length > 0) {
+			btn[0].click();
+		}
+	}
+};
 
+var setPlaybackRate = function(rate) {
+	console.log("setting rate : " + rate);
+	if (rate) {
+		settedRate = rate;
+		player.playbackRate = rate;
+	}
+	else if (settedRate) {
+		player.playbackRate = settedRate
+	}
+};
+
+var onNewTrack = function() {
+	// autoplay, with UI
+	if (!startedPlay) {
+		startedPlay = true;
+		$pp();
+	}
+	// make sure we didn't unset the playback rate
+	setPlaybackRate();
+	
+	// notification builder
+	var story = document.getElementsByTagName('article');
+	if (story.length > 0) {
+		story = story[0];
+		var title = story.getElementsByClassName('card__title');
+		title = title.length > 0 ? title[0].innerText : null;
+		console.log(title);
+		if (!title || title.indexOf("our sponsor") != -1) {return;}
+		var img = story.getElementsByTagName('img');
+		img = img.length > 0 ? img[0].src : null;
+		console.log(img);
+		// story.getElementsByTagName('time')
+		var slug = story.getElementsByClassName('card__slug');
+		slug = slug.length > 0 ? slug[0].innerText : null;
+		var metas = story.getElementsByClassName('card__meta');
+		var message = slug ? slug : "";
+		for (let m of metas) {
+			var text = m.innerText.replace(/\s+/g,' ').trim();
+			if (text) {
+				if (message.length > 0) { message += " • " }
+				message += text;
+			}
+		}
+
+		var options = {
+		  "type": "basic",
+		  "title": title || message,
+		  "message": message || title,
+		};
+		
+		if (img && img.indexOf("img/fallbacks") == -1) {
+			options.type = "image";
+			options.imageUrl = img;
+		}
+		
+		var reader = document.getElementsByTagName('read-button');
+		var story_url;
+		if (reader.length > 0) {
+			reader = reader[0];
+			var link = reader.getElementsByTagName('a');
+			if (link.length > 0) {
+				story_url = link[0].href;
+			}
+		}
+		
+		chrome.runtime.sendMessage({"action":"notification", "options": options, "story_url": story_url});
+	}
+};
+
+chrome.storage.sync.get({'speed': 1}, function(items) {setPlaybackRate(items.speed) });
+
+player.onloadeddata = onNewTrack;
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+	if (request.action == "speed") {
+		setPlaybackRate(request.speed);
+	}
+	else if (request.action == "playpause") {
+		$pp();
+	}
+	else if (request.action == "skip") {
+		$skip();
+	}
+});
+
+player.onplay = function() { chrome.runtime.sendMessage({"action":"onplay"}) };
+player.onpause = function() { chrome.runtime.sendMessage({"action":"onpause"}) };
